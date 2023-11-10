27e74d6b10ef569b2f12436a860c0a8f8fae0a6e
ashpak-p <145493302+Ashpak-p@users.noreply.github.com>
2023-11-11 00:20:37 +0600

    Delete modern-http directory

--- modern-http/drizzle/0000_fine_ironclad.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `notes` (
-	`id` bigint AUTO_INCREMENT NOT NULL,
-	`text` text NOT NULL,
-	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT `notes_id` PRIMARY KEY(`id`)
-);
--- modern-http/read.md
+++
@@ -1,4 +0,0 @@
-{
-  "text": "My note in MySQL"
-  "date": 1697915008129
-}
